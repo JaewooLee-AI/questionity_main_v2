10a496afdd1d3475800ef2c630265f85805c1c25
Jaewoo Lee <jwlee@Jaewooui-MacBookPro.local>
2026-08-20 11:43:37 +0900

    style: reduce bottom padding of CURATED ALADIN 300 BOOKS section to pb-6 md:pb-8

--- src/pages/home/components/CategorySection.tsx
+++ src/pages/home/components/CategorySection.tsx
@@ -211,7 +211,7 @@ export default function CategorySection() {
   };
 
   return (
-    <section id="categories" className="w-full px-6 md:px-12 lg:px-20 pt-6 pb-16 md:pt-8 md:pb-20 bg-[#f4f3ee] border-t border-[#1a1a1a]/10 relative">
+    <section id="categories" className="w-full px-6 md:px-12 lg:px-20 pt-6 pb-6 md:pt-8 md:pb-8 bg-[#f4f3ee] border-t border-[#1a1a1a]/10 relative">
       <div className="max-w-7xl mx-auto">
         {/* Section Title Header */}
         <div className="text-center mb-8 md:mb-10">
